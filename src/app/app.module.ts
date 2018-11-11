41ec4f0edb71bfd21cfd4846bd40dc26f6d447ac
Nadine Frieß <nadine.friess@gmail.com>
2018-11-11 12:17:33 +0100

    fix dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { RecipesComponent } from "./recipes/recipes.component";
-import { NO_ERRORS_SCHEMA } from "@angular/compiler/src/core";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, RecipesComponent],
